a9d202a4d6d829b7fc83ece24d6bfbdbc2528c3c
bendeveloperpro <lavaudbenoitpro@gmail.com>
2025-10-08 00:13:29 +0200

    Add child theme files

---
+++ functions.php
@@ -0,0 +1,15 @@
+﻿<?php
+/**
+ * Twenty Twenty-Five Child theme functions.
+ */
+function twentytwentyfive_child_enqueue_styles() {
+    $parent_handle = 'twentytwentyfive-style';
+
+    wp_enqueue_style(
+        'twentytwentyfive-child-style',
+        get_stylesheet_uri(),
+        array( $parent_handle ),
+        wp_get_theme()->get( 'Version' )
+    );
+}
+add_action( 'wp_enqueue_scripts', 'twentytwentyfive_child_enqueue_styles' );
